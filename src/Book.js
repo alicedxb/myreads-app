f7d11c5c1404ed78720d7688c1718a917883bd44
Alice <32536678+alicedxb@users.noreply.github.com>
2018-07-14 23:46:52 +0400

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -15,11 +15,11 @@ const Book = ({ infos, updateBook }) => {
                 </div>
 				<div className="book-shelf-changer">
 					<select value={book.shelf} onChange={(e) => updateBook(book, e.target.value)}>
-						<option value="none" disabled>Move to...</option>
+						<option value="" disabled>Move to...</option>
+                                                <option value="none">None</option>
 						<option value="currentlyReading">Currently Reading</option>
 						<option value="wantToRead">Want to Read</option>
 						<option value="read">Read</option>
-						<option value="none">None</option>
 					</select>
 				</div>
 			</div>
