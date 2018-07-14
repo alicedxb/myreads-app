e37b9923f6487df37eca8775aa62efadb291444f
Alice <32536678+alicedxb@users.noreply.github.com>
2018-07-14 17:22:04 +0400

    Create Header.js

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+	return (
+		<div className="list-books">
+			<div className="list-books-title">
+				<h1>MyReads App</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
