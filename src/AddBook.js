bd93f302f7606ce753ba244f82f4ce7ab2755ba5
Alice <32536678+alicedxb@users.noreply.github.com>
2018-07-14 18:19:50 +0400

    Update AddBook.js

--- src/AddBook.js
+++ src/AddBook.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 
+// Accesses the search function
 const AddBook = () => {
 	return (
 		<div className="open-search">
